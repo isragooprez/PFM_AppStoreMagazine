69f375c49aadcc7add26592d9c2274b964788215
Santiago Israel Pérez Auqui <isragoo.prez@hotmail.com>
2018-05-07 14:41:37 +0200

    Init commit Configuracion de entorno

---
+++ Magazine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Magazine.Startup))]
+namespace Magazine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
